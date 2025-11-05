918362401cfe4f2def663e7f3461428c9eb727d9
Ashutoshkj <jashutosh528@gmail.com>
2025-11-05 13:48:40 +0530

    MusicJunction-Music-Entertainment-Platform

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+
+const express=require("express");
+const dotenv=require("dotenv");
+
+ const cors =require("cors");
+const connectDB=require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+   res.send("🎵 Welcome to MusicJunction API");
+});
+const PORT=process.env.PORT || 8085;
+
+app.listen(PORT,()=>{
+    console.log(`🚀 Server running on port ${PORT}`);
+})
